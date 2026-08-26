b74543a9f8537a4fa57e2adbda88647a7501fc9b
jessicaangelina7002 <jessicaangelina7002@gmail.com>
2026-08-26 21:39:08 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ model = joblib.load("model_kredit.pkl")
 st.title("Aplikasi Persetujuan Kredit Based on AI Model")
 st.write("Aplikasi ini memprediksi apakah pengajuan kredit disetujui atau tidak.")
 
-with st.form("Form Credit"):
+with st.form("Form Credit")
     #2. membuat form input untuk user kita
     gaji = st.number_input("Gaji Bulanan (Juta Rupiah)", min_value=1, max_value=30, value=5)
     umur = st.number_input("Umur", min_value=17, max_value=100, value=25)
     cicilan = st.number_input("cicilan di bank lain (juta rupiah): ", min_value = 0, value = 1)
 
-submit_button = st.form_submit_button("prediksi status kredit")
+    submit_button = st.form_submit_button("prediksi status kredit")
 
 if submit_button:
     data_baru = pd.DataFrame({
