6a21f05683f8acaf0749fed3d9c09f4ef247eaa7
jessicaangelina7002 <jessicaangelina7002@gmail.com>
2026-08-26 20:11:48 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -9,10 +9,11 @@ model = joblib.load("model_kredit.pkl")
 st.title("Aplikasi Persetujuan Kredit Based on AI Model")
 st.write("Aplikasi ini memprediksi apakah pengajuan kredit disetujui atau tidak.")
 
-#2. membuat form input untuk user kita
-gaji = st.number_input("Gaji Bulanan (Juta Rupiah)", min_value=1, max_value=30, value=5)
-umur = st.number_input("Umur", min_value=17, max_value=100, value=25)
-cicilan = st.number_input("cicilan di bank lain (juta rupiah): ", min_value = 0, value = 1)
+with st.form("Form Credit")
+    #2. membuat form input untuk user kita
+    gaji = st.number_input("Gaji Bulanan (Juta Rupiah)", min_value=1, max_value=30, value=5)
+    umur = st.number_input("Umur", min_value=17, max_value=100, value=25)
+    cicilan = st.number_input("cicilan di bank lain (juta rupiah): ", min_value = 0, value = 1)
 
 submit_button = st.form_submit_button("prediksi status kredit")
 
@@ -26,7 +27,7 @@ if submit_button:
 hasil= model.predict(data.baru)[0]
 
 st.markdown("----")
-if(hasil = 1):
+if hasil == 1:
     st.success("selamat pengajuan kredit anda kemungkinan di terima")
 else:
     st.error("yahh maaf pengajuan kredit anda di tolak")
